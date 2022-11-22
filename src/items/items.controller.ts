d749a9af6c80bde218cb0b025b6e1a3b6204f7a6
fo-ji <fumiya1753@gmail.com>
2022-11-22 18:07:33 +0900

    add item crud

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,12 +1,49 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { ItemsService } from './items.service';
+import { Item } from './items.model';
+import { ItemStatus } from './item-status.enum';
 
 @Controller('items')
 export class ItemsController {
   constructor(private readonly itemsService: ItemsService) {}
 
   @Get()
-  findAll() {
+  findAll(): Item[] {
     return this.itemsService.findAll();
+  }
+
+  @Get(':id')
+  findById(@Param('id') id: string): Item {
+    return this.itemsService.findById(id);
+  }
+
+  @Post()
+  create(
+    @Body('id') id: string,
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description: string,
+  ): Item {
+    const item = { id, name, price, description, status: ItemStatus.ON_SALE };
+
+    return this.itemsService.create(item);
+  }
+
+  @Patch(':id')
+  updateStatus(@Param('id') id: string): Item {
+    return this.itemsService.updateStatus(id);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string): void {
+    return this.itemsService.delete(id);
   }
 }

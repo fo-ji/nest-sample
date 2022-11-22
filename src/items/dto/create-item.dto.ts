5610074992c8f8ead57023b650d836de49109467
fo-ji <fumiya1753@gmail.com>
2022-11-22 18:48:53 +0900

    add class-validator and class-transformer

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,5 +1,19 @@
+import { Type } from 'class-transformer';
+import { IsInt, IsNotEmpty, IsString, MaxLength, Min } from 'class-validator';
+
 export class CreateItemDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(40)
   name: string;
+
+  @IsInt()
+  @Min(1)
+  // MEMO: class-validatorでバリデーションする前に定義した型に変換する
+  @Type(() => Number) // MEMO: class-transformerで変換される
   price: number;
+
+  @IsString()
+  @IsNotEmpty()
   description: string;
 }

27550b9b46781944284ec865d805c2f22e73b894
fo-ji <fumiya1753@gmail.com>
2022-11-23 18:16:58 +0900

    implement login

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from 'src/entities/user.entity';
+import { UserRepository } from './user.repository';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private userRepository: UserRepository) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false, // MEMO: tokenの期限切れを許容するか
+      secretOrKey: 'secretKey123',
+    });
+  }
+
+  async validate(payload: { id: string; username: string }): Promise<User> {
+    const { id, username } = payload;
+    const user = await this.userRepository.findOne({ id, username });
+
+    if (user) return user;
+    throw new UnauthorizedException();
+  }
+}
